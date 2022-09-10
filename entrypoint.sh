b4a0ce4c4c328b21985052d297bb42117ddafce7
Jacob M <jaymuk.business@gmail.com>
2022-09-09 21:27:12 -0500

    initital commit

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "Debugging stuff"
+
+IFS=$'\n'
+
+for v in `env`; do
+  PREFIX=$(echo $v | cut -d '_' -f1)
+
+  if [[ $PREFIX == 'GITHUB' ]] || [[ $PREFIX == 'INPUT' ]]; then
+    echo $v;
+  fi
+done
+
+echo '-----'
+echo 'Event:'
+
+jq $INPUT_JQ_SELECTOR $GITHUB_EVENT_PATH
